079f2252d9b3e8ddbc64e6f352eb3fd8218b4e42
Piotr Lugowski <s16481@pja.edu.pl>
2020-03-01 17:46:28 +0100

    Scraper

--- Cw1/Program.cs
+++ Cw1/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Cw1
 {
     class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            foreach(var a in args)
+            {
+                Console.WriteLine(a);
+            }
+            //var emails = await GetEmails(args[0]);
+            var emails = await GetEmails("https://www.pja.edu.pl/");
+            foreach(var email in emails)
+            {
+                Console.WriteLine(email);
+            }
         }
+
+        static async Task<IList<string>> GetEmails(string url)
+        {
+            var httpClient = new HttpClient();
+            var listOfEmails = new List<string>();
+            var response = await httpClient.GetAsync(url);
+            Regex emailRegex = new Regex(@"\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*", RegexOptions.IgnoreCase);
+            MatchCollection emailMatches = emailRegex.Matches(response.Content.ReadAsStringAsync().Result);
+            foreach (var emailMatche in emailMatches)
+            {
+                listOfEmails.Add(emailMatche.ToString());
+            }
+            return listOfEmails;
+        } 
     }
 }
